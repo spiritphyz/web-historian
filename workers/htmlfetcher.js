515e56eda35f092b2044c2aec3a2e30601641177
spiritphyz <heytonyle@icloud.com>
2016-10-06 16:47:17 -0700

    Implement a node shell script through crontab
    - crontab contents:
    * * * * * /Users/student/Desktop/2016-09-web-historian/workers/htmlfetcher.js >/Users/student/Desktop/2016-09-web-historian/workers/watcher.log
    * * * * * ( sleep 10 ; /Users/student/Desktop/2016-09-web-historian/workers/htmlfetcher.js >/Users/student/Desktop/2016-09-web-historian/workers/watcher.log )
    * * * * * ( sleep 20 ; /Users/student/Desktop/2016-09-web-historian/workers/htmlfetcher.js >/Users/student/Desktop/2016-09-web-historian/workers/watcher.log )
    * * * * * ( sleep 30 ; /Users/student/Desktop/2016-09-web-historian/workers/htmlfetcher.js >/Users/student/Desktop/2016-09-web-historian/workers/watcher.log )
    * * * * * ( sleep 40 ; /Users/student/Desktop/2016-09-web-historian/workers/htmlfetcher.js >/Users/student/Desktop/2016-09-web-historian/workers/watcher.log )
    * * * * * ( sleep 50 ; /Users/student/Desktop/2016-09-web-historian/workers/htmlfetcher.js >/Users/student/Desktop/2016-09-web-historian/workers/watcher.log )
    
    - commands to edit crontab:
      - crontab -e
      - crontab -l
    
    - commands to make shell script executable:
      - chmod a+x /Users/student/Desktop/2016-09-web-historian/workers/htmlfetcher.js

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,2 +1,22 @@
+#!/usr/local/bin/node
+
 // Use the code in `archive-helpers.js` to actually download the urls
 // that are waiting.
+var path = require('path');
+var fs = require('fs');
+var archive = require('../helpers/archive-helpers');
+
+archive.readListOfUrls(function(data) {
+  data.forEach(function(url) {
+    archive.isUrlArchived(url, function(exists) {
+      if (!exists) {
+        archive.downloadUrls([url]);
+        // do nothing
+      }
+    });
+  });
+
+});
+
+fs.appendFile(path.join(__dirname, '../workers/watcher.log'), 'Added archived website at time ' + new Date() + '\n');
+
